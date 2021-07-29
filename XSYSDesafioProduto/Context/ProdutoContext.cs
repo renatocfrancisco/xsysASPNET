4b8a184522551a86249b785d9627adbf4c350db0
renatocfrancisco <renatocfrancisco@alunos.fho.edu.br>
2021-07-29 19:34:16 -0300

    Adicionar arquivos de projeto.

---
+++ XSYSDesafioProduto/Context/ProdutoContext.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using XSYSDesafioProduto.Models;
+
+namespace XSYSDesafioProduto.Context
+{
+    public class ProdutoContext : DbContext
+    {
+        public ProdutoContext(DbContextOptions<ProdutoContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Produto> Produtos { get; set; }
+    }
+}
---
+++ XSYSDesafioProduto/Models/Produto.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace XSYSDesafioProduto.Models
+{
+    public class Produto
+    {
+        public int Id { get; set; }
+
+        [Required(ErrorMessage = "Coloque um nome/Descreva o Produto")]
+        [DisplayName("Descrição")]
+        public string Descricao { get; set; }
+        [Required(ErrorMessage = "Quantos tem no estoque?")]
+
+        public int Estoque { get; set; }
+
+        [Required(ErrorMessage = "Insira um preço!")]
+        [DisplayName("Preço")]
+        public decimal Preco { get; set; }
+    }
+}
