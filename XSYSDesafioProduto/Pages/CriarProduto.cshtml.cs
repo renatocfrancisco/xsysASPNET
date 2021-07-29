d0c2fdd24fcaaa02620d7a4408ab8c6589646dfc
renatocfrancisco <renatocfrancisco@alunos.fho.edu.br>
2021-07-29 20:15:32 -0300

    um detalhe

---
+++ XSYSDesafioProduto/Pages/CriarProduto.cshtml.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using XSYSDesafioProduto.Context;
+using XSYSDesafioProduto.Models;
+
+namespace XSYSDesafioProduto.Pages
+{
+    public class CriarProdutoModel : PageModel
+    {
+        private readonly XSYSDesafioProduto.Context.ProdutoContext _context;
+
+        public CriarProdutoModel(XSYSDesafioProduto.Context.ProdutoContext context)
+        {
+            _context = context;
+        }
+
+        public IActionResult OnGet()
+        {
+            return Page();
+        }
+
+        [BindProperty]
+        public Produto Produto { get; set; }
+
+        // To protect from overposting attacks, see https://aka.ms/RazorPagesCRUD
+        public async Task<IActionResult> OnPostAsync()
+        {
+            if (!ModelState.IsValid)
+            {
+                return Page();
+            }
+
+            _context.Produtos.Add(Produto);
+            await _context.SaveChangesAsync();
+
+            return RedirectToPage("./Index");
+        }
+    }
+}
---
+++ XSYSDesafioProduto/Pages/ListaProdutos.cshtml.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+using XSYSDesafioProduto.Context;
+using XSYSDesafioProduto.Models;
+
+namespace XSYSDesafioProduto.Pages
+{
+    public class ListaProdutosModel : PageModel
+    {
+        private readonly XSYSDesafioProduto.Context.ProdutoContext _context;
+
+        public ListaProdutosModel(XSYSDesafioProduto.Context.ProdutoContext context)
+        {
+            _context = context;
+        }
+
+        public IList<Produto> Produto { get;set; }
+
+        public async Task OnGetAsync()
+        {
+            Produto = await _context.Produtos.ToListAsync();
+        }
+    }
+}
